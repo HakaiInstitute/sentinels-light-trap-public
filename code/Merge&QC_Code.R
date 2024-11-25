4871836d40aeb93563533a14f5e7e5ddde084922
hakai-it <null>
2024-11-25 17:13:30 +0000

    🔄 synced local 'code/Merge&QC_Code.R' with remote 'code/Merge&QC_Code.R'

--- code/Merge&QC_Code.R
+++ code/Merge&QC_Code.R
@@ -1,5 +1,5 @@
 #===============================================================================
-# Sentinels Larval Crab Monitoring | Light Trap Abundance Data
+# Sentinels Larval Crab Monitoring | Light Trap Abundance and Size Data
 # Merge data from different years and run QAQC codes to produce master dataset
 # Filter out sensitive data for public facing repository (i.e. sites that 
 # don't want data published yet)
@@ -8,16 +8,19 @@
 # Input:
 #     2022_CountData_QC.csv
 #     2023_CountData_QC.csv
+#     2024_CountData_QC.csv
+#     2023_Megalopae_Carapace_Widths.csv
+#     2024_Megalopae_Carapace_Widths.csv
 #
 # Output:
 #     Master_QAQC_LT_counts.csv
 #     Master_QAQC_LightTrap_Counts_publicrepository.csv
-#     2023_Megalopae_Carapace_Widths.csv
+#     Master_QAQC_CarapaceWidth_Measurements.csv
 #     Master_QAQC_CarapaceWidth_Measurements_publicrepository.csv
 #
 # Heather Earle (Hakai Institute) heather.earle@hakai.org
 # First Created 11/2023
-# Last Updated 09/2024
+# Last Updated 11/2024
 #===============================================================================
 
 #clean up Enivronment
@@ -64,7 +67,7 @@ counts_master <- merge(counts_all,stations,by=c("Site"))
 
 # For annual reports and Hakai Data Catalogue, remove all MET, DNF, ERR, & INC
 # entries:
-counts_QC <- counts_master %>%  filter(Error_Code=="NA" | Error_Code== "HRS" 
+counts_QC <- counts_master %>%  filter(Error_Code== "None" | Error_Code== "HRS" 
                                         | Error_Code == "BAT" | Error_Code == "SUB")
 ###note, here I added teh "SUB" error code so that the Whaler Bay subsampled 
 #numbers from late June/early July 2024 would still be included in the fall gathering
@@ -78,9 +81,22 @@ counts_removed <- counts_master %>% filter (Error_Code =="DNF" |
                                             Error_Code == "INC")
 
 
-#==== MASTER for GITHUB ========================================================
+#====FORMATED MEASUREMENT DATA =================================================
 
 
+#upload measurement data from individual years
+measurements23 <- read_csv("data/2023/2023_Megalopae_Carapace_Widths.csv")
+measurements24 <- read_csv("data/2024/2024_Megalopae_Carapace_Widths.csv")
+measurements24 <- select(measurements24, -submission_id, -measured_by,
+                         -photo_comments) #remove columns from rough dataset that don't match
+
+#combine years
+measurements_all <- rbind(measurements23, measurements24)
+
+
+#==== MASTER Datasets for Internal GITHUB ======================================
+
+####COUNTS#####
 # Select columns for dataframe that will be on internal github
 counts_master <- counts_QC %>%
  dplyr:: select(Code, Site, Lat, Lon, Year, Month, Date, 
@@ -90,9 +106,19 @@ counts_master <- counts_QC %>%
          TotalMmagister = TotalMmagister, 
          CPUE_Night, CPUE_Hour, Error_Code)
 
+##create csv
 write_csv(counts_master, "data/Master_QAQC_LightTrap_Counts.csv")
 
-#===== MASTER for PUBLIC GITHUB ================================================
+##create csv of removed entries
+write_csv(measurements_all, "data/Master_QAQC_Carapace_Width_Measurements.csv")
+
+#####MEASUREMENTS#####
+ write_csv(counts_removed, "data/Master_Removed_Counts.csv")
+
+
+#===== MASTER Datasets for PUBLIC GITHUB =======================================
+
+#####COUNTS#####
 
 ##Remove sites requiring further permissions and with incomplete data
 counts_master_p <- counts_master %>%
@@ -100,19 +126,18 @@ counts_master_p <- counts_master %>%
           & Code != "BOO" & Code != "LYA" & Code != "WIN")
 
 
-write_csv(counts_master_p, "data/Master_QAQC_LightTrap_Counts_publicrepository.
-          csv")
+write_csv(counts_master_p, "data/Master_QAQC_LightTrap_Counts_publicrepository.csv")
 
-#===== 2023 Measurements for PUBLIC GITHUB =====================================
+#####MEASUREMENTS#####
 
-meas23 <- read_csv("data/2023/2023_Megalopae_Carapace_Widths.csv")
+##Remove sites requiring further permissions and with incomplete data
+measurements_all <- measurements_all %>%
+  dplyr::filter(site != "Pender Harbour" & site != "Sechelt Inlet" & site != 
+                  "Powell River" & site != "Boot Cove" & site != "Lyall Harbour"
+                & site != "Winter")
 
-meas23p <- meas23 %>%
- dplyr::filter(site != "Pender Harbour" & site != "Sechelt Inlet" & site != 
-                 "Powell River" & site != "Boot Cove" & site != "Lyall Harbour"
-               & site != "Winter")
+#write csv for public repository
+write_csv(measurements_all, "data/Master_QAQC_CarapaceWidth_Measurements_publicrepository.csv")
 
-write_csv(meas23p, "data/Master_QAQC_CarapaceWidth_Measurements_publicrepository.
-          csv")
 #================================ END ==========================================
 
